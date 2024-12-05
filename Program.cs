12a01c81b66514f781e92e33ef0a344c653016b9
Aeezin <gullestrand@gmail.com>
2024-12-05 11:49:19 +0100

    whiles and switches

--- Program.cs
+++ Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool loginMenuRunning = true;
+        bool transactionMenu = true;
+
+        string? userChoice = Console.ReadLine();
+
+        while (loginMenuRunning)
+        {
+            switch (userChoice)
+            {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+                    loginMenuRunning = false;
+                    break;
+            }
+        }
+
+        while (transactionMenu)
+        {
+            switch (userChoice)
+            {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+
+                    break;
+                case "5":
+
+                    break;
+                case "6":
+                
+                    break;
+            }
+        }
     }
 }
