449228e3d9196ac3386390ed965383b997a1779d
Aeezin <Gullestrand@gmail.com>
2024-12-06 12:44:24 +0100

    Menues

--- Program.cs
+++ Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool loginMenuRunning = true;
+        bool transactionMenuRunning = true;
+
+        string userChoice = Console.ReadLine();
+
+        while (loginMenuRunning)
+        {
+            LoginMenu.Execute();
+
+            switch (userChoice)
+            {
+                case "1":
+                    // Logga in
+                    break;
+                case "2":
+                    // Skapa account
+                    break;
+                case "3":
+                    loginMenuRunning = false;
+                    transactionMenuRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.");
+                    break;
+            }
+        }
+
+        while (transactionMenuRunning)
+        {
+            TransactionMenu.Execute();
+
+            switch (userChoice)
+            {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+
+                    break;
+                case "5":
+
+                    break;
+                case "6":
+                    
+                    transactionMenuRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.");
+            }
+        }
     }
 }
