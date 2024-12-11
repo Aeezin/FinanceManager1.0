850bac8d9868060cd564c0f471bf2d44092a8e97
Aeezin <gullestrand@gmail.com>
2024-12-11 19:03:09 +0100

    create account and login

--- Program.cs
+++ Program.cs
@@ -7,24 +7,16 @@ class Program
 
     static void Main(string[] args)
     {
-<<<<<<< HEAD
         Console.Clear();
         bool loginMenuRunning = true;
         bool transactionMenuRunning = false;
 
         string userChoice;
-=======
-        bool loginMenuRunning = true;
-        bool transactionMenuRunning = true;
-
-        string userChoice = Console.ReadLine();
->>>>>>> 449228e3d9196ac3386390ed965383b997a1779d
 
         while (loginMenuRunning)
         {
             LoginMenu.Execute();
 
-<<<<<<< HEAD
             userChoice = Console.ReadLine()!;
 
             switch (userChoice)
@@ -36,29 +28,10 @@ class Program
                 case "2":
                     Console.Clear();
                     Account.Login();
-                    if (Account.Login().Equals(true))
-                    {
-                        transactionMenuRunning = true;
-                    }
+                    // logik
                     break;
                 case "3":
                     loginMenuRunning = false;
-=======
-            switch (userChoice)
-            {
-                case "1":
-                    // Logga in
-                    break;
-                case "2":
-                    // Skapa account
-                    break;
-                case "3":
-                    loginMenuRunning = false;
-                    transactionMenuRunning = false;
-                    break;
-                default:
-                    Console.WriteLine("Invalid input. Please try again.");
->>>>>>> 449228e3d9196ac3386390ed965383b997a1779d
                     break;
             }
         }
@@ -67,11 +40,8 @@ class Program
         {
             TransactionMenu.Execute();
 
-<<<<<<< HEAD
             userChoice = Console.ReadLine()!;
 
-=======
->>>>>>> 449228e3d9196ac3386390ed965383b997a1779d
             switch (userChoice)
             {
                 case "1":
@@ -90,16 +60,8 @@ class Program
 
                     break;
                 case "6":
-<<<<<<< HEAD
                     transactionMenuRunning = false;
                     break;
-=======
-                    
-                    transactionMenuRunning = false;
-                    break;
-                default:
-                    Console.WriteLine("Invalid input. Please try again.");
->>>>>>> 449228e3d9196ac3386390ed965383b997a1779d
             }
         }
     }
